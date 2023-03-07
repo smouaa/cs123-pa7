681fccb441cf195afd5070fba82fd1e42c90af18
Katie Liu <122421205+katieliuu@users.noreply.github.com>
2023-03-07 15:33:22 -0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ class Chatbot:
     def __init__(self, creative=False):
         # The chatbot's default name is `moviebot`.
         # TODO: Give your chatbot a new name.
-        self.name = 'Testinggg'
+        self.name = 'Bert'
 
         self.creative = creative
 
